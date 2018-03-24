11f3bc2cd809378d77ea7f533c96889230bd0665
Rio Matsuoka <17266508+riomat13@users.noreply.github.com>
2018-03-24 02:34:27 -0700

    added file rename function

--- helper.py
+++ helper.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 import os
 import shutil
+from glob import glob
 
 
 def mkdirs(dir):
@@ -10,3 +12,20 @@ def mkdirs(dir):
 def rmdirs(dir):
     if os.path.exists(dir):
         shutil.rmtree(dir)
+
+
+def rename_files(dir, prefix, img_type="png"):
+    paths = os.path.join(dir, '*.' + img_type)
+    files = glob(paths)
+    assert len(files) > 0, "No file is found."
+
+    os.mkdir(os.path.join(dir, "renamed"))
+    files = sorted(files)
+
+    if type(prefix) == int:
+        prefix = "{:02d}".format(prefix)
+
+    for n, f in enumerate(files):
+        os.rename(f, os.path.join(dir, "renamed", "{}_{:04d}.".format(prefix, n) + img_type))
+
+    print("Total number of files : {}".format(n+1))
